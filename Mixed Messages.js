89df18e7399c71e5e35a8c2f910b22974ae77abd
David Ferrera <dev.ferrera@gmail.com>
2021-04-01 22:17:14 +0200

    small correction on jokes

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -4,7 +4,7 @@ const jokes = {
     joke: ['Why did Wi-Fi and the computer get married? because they had a connection', 
         'I love the smell of my F5 key. – It is very refreshing.', 
         'Why was the computer so good at golf? because he had a hard drive',
-        'A programmer’s wife tells him, - While you’re at the grocery store, buy some eggs. He never comes back.',
+        'A programmer’s wife tells him, "While you’re at the grocery store, buy some eggs" He never comes back.',
         'Why was the JavaScript developer sad? Because he didn´t Node how to Express himself',
         'Chuck Norris can take a screenshot of his blue screen',
         'A foo walks into a bar, takes a look around and says ´Hello World´'],
